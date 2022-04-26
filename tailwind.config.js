bf4ee0cc75c5347259f228a93116054f640d860f
iammutevu <ianmutevu96@gmail.com>
2022-04-26 17:41:12 +0300

    Add borders, margins, and padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   mode: 'jit',
+  purge: [
+     './public/*.html',
+     './src/*.{js,jsx,ts,tsx,vue}',
+   ],
   content: [
     './public/index.html',
     './src/**/*.{html,js}'
